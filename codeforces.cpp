b2f1fb001b03b5a011b90c9587798c1812d1eb40
Timur Sizov <tsizov24@gmail.com>
2020-12-25 12:43:50 +0500

    Made some changes

--- codeforces.cpp
+++ codeforces.cpp
@@ -1,49 +1,70 @@
 #include <bits/stdc++.h>
 #include <chrono>
- 
+
 using namespace std;
 using namespace std::chrono;
- 
+
 typedef long long ll;
 typedef long double ld;
- 
+
 int MOD = 1e9 + 7;
- 
+
 ll getTime() {
     return duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
 }
- 
+
 bool yes(bool res, bool isUpperCase = false) {
     if (isUpperCase) cout << (res ? "YES" : "NO") << "\n";
     else cout << (res ? "Yes" : "No") << "\n";
     return res;
 }
- 
+
+ll res;
+
+void init() {
+    res = 0;
+}
+
+void input() {
+
+}
+
+void count() {
+
+}
+
+void output() {
+    cout << res << "\n";
+}
+
 void solve() {
-    int t;
-    cin >> t;
-    // t = 1;
-    while(t--) {
-    	
+    int T;
+    cin >> T;
+    // T = 1;
+    while(T--) {
+        init();
+        input();
+        count();
+        output();
     }
 }
 
 void test() {
-    int t;
-    cin >> t;
-    // t = 1;
-    while (t--) {
-        
+    int T;
+    cin >> T;
+    // T = 1;
+    while(T--) {
+
     }
 }
 
 int main() {
-//    freopen("input.txt", "r", stdin);
-//    freopen("output.txt", "w", stdout);
- 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     solve();
     // test();
 }
--- google.cpp
+++ google.cpp
@@ -1,45 +1,70 @@
 #include <bits/stdc++.h>
 #include <chrono>
- 
+
 using namespace std;
 using namespace std::chrono;
- 
+
 typedef long long ll;
 typedef long double ld;
- 
+
 int MOD = 1e9 + 7;
- 
+
 ll getTime() {
     return duration_cast< milliseconds >(system_clock::now().time_since_epoch()).count();
 }
- 
+
 bool yes(bool res, bool isUpperCase = false) {
     if (isUpperCase) cout << (res ? "YES" : "NO") << "\n";
     else cout << (res ? "Yes" : "No") << "\n";
     return res;
 }
- 
+
+ll res;
+
+void init() {
+    res = 0;
+}
+
+void input() {
+
+}
+
+void count() {
+
+}
+
+void output(int caseNum) {
+    cout << "Case #" << caseNum << ": " << res << "\n";
+}
+
 void solve() {
     int T;
     cin >> T;
-    // t = 1;
+    // T = 1;
     for (int i0 = 1; i0 <= T; i0++) {
-        
-        cout << "Case #" << i0 << ": " << "\n";
+        init();
+        input();
+        count();
+        output(i0);
     }
 }
 
 void test() {
+    int T;
+    cin >> T;
+    // T = 1;
+    while(T--) {
 
+    }
 }
 
 int main() {
-//    freopen("input.txt", "r", stdin);
-//    freopen("output.txt", "w", stdout);
- 
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
- 
+
     solve();
     // test();
 }
